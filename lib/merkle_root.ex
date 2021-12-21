32d72c8bc743a61d90800effe44493ce1a429eaa
arpple <apple0239@gmail.com>
2021-12-21 21:04:57 +0000

    root

--- lib/merkle_root.ex
+++ lib/merkle_root.ex
@@ -1,3 +1,38 @@
 defmodule MerkleRoot do
-  def get([]), do: ""
+  def from_file(path) do
+    File.stream!(path)
+    |> get()
+  end
+
+  def get(transactions) do
+    hashed_pairs = transactions
+    |> Enum.chunk_every(2)
+    |> Enum.map(&hash_transactions/1)
+
+    if is_root(hashed_pairs) do
+      Enum.at(hashed_pairs, 0)
+    else
+      get(hashed_pairs)
+    end
+  end
+
+  defp hash_transactions([a]) do
+    hash_transactions([a, a])
+  end
+
+  defp hash_transactions([a, b]) do
+    hashed = a <> b
+    |> hash()
+    |> hash()
+  end
+
+  defp hash(str) do
+    :crypto.hash(:sha256, str)
+    |> Base.encode16(case: :lower)
+  end
+
+  defp is_root(pairs) do
+    check = Enum.take(pairs, 2)
+    length(check) == 1
+  end
 end
--- test/merkle_root_test.exs
+++ test/merkle_root_test.exs
@@ -2,10 +2,6 @@ defmodule MerkleRootTest do
   use ExUnit.Case
   doctest MerkleRoot
 
-  test "no transaction should be empty" do
-    assert MerkleRoot.get([]) == ""
-  end
-
   test "non pair transaction should be duplicate as pair" do
     single_transactions = ["11"]
     duplicated_transactions = ["11", "11"]
@@ -13,7 +9,7 @@ defmodule MerkleRootTest do
     assert MerkleRoot.get(single_transactions) == MerkleRoot.get(duplicated_transactions)
   end
 
-  test "example transactions" do
+  test "try calculate with some transactions" do
     transactions = [
       "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87",
       "fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4",
@@ -21,8 +17,6 @@ defmodule MerkleRootTest do
       "e9a66845e05d5abc0ad04ec80f774a7e585c6e8db975962d069a522137b80c1d",
     ]
 
-    expected = "f3e94742aca4b5ef85488dc37c06c3282295ffec960994b2c0d5ac2a25a95766"
-
-    assert MerkleRoot.get(transactions) == expected
+    MerkleRoot.get(transactions)
   end
 end
